601632f1a5b2e161f2da5bb153658d69da7898cc
draplater <tsjzadmin@gmail.com>
2016-01-16 21:20:05 +0800

    AuthActivity: init; preference: init

---
+++ mobile/src/main/java/com/luolc/uberwalker/AuthActivity.java
@@ -0,0 +1,84 @@
+package com.luolc.uberwalker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.luolc.uberwalker.Manager.ApiManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class AuthActivity extends AppCompatActivity {
+
+    WebView webview;
+    private SharedPreferences pref;
+
+    class WebAppConnector {
+        Context mContext;
+        /** Instantiate the interface and set the context */
+        WebAppConnector(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void setUid(String uid) {
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("uid", uid);
+            editor.apply();
+            finish();
+        }
+    }
+
+    private void getAuthURL() {
+        ApiManager api = ApiManager.getInstance();
+        api.get("login", null,
+                new Response.Listener<String>() {
+            @Override
+            public void onResponse(String resp) {
+                try {
+                    JSONObject obj = new JSONObject(resp);
+                    String authURL = obj.getString("auth_url");
+                    webview.loadUrl(authURL);
+                } catch (JSONException e) { // TODO
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError e) { // TODO
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        setContentView(R.layout.activity_auth);
+        webview = (WebView) findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(false);
+
+        webview.addJavascriptInterface(new WebAppConnector(this), "Android");
+        getAuthURL();
+    }
+}
+;
