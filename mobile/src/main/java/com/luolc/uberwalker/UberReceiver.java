cd9a95ee19c68ba8420250ef16e94bc64bc4a57f
draplater <tsjzadmin@gmail.com>
2016-01-17 13:51:08 +0800

    uberreceiver: init

---
+++ mobile/src/main/java/com/luolc/uberwalker/UberReceiver.java
@@ -0,0 +1,36 @@
+package com.luolc.uberwalker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.avos.avoscloud.LogUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+public class UberReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "UberReceiver";
+
+    public UberReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LogUtil.log.d(TAG, "Get Broadcat");
+        try {
+            String action = intent.getAction();
+            String channel = intent.getExtras().getString("com.avos.avoscloud.Channel");
+            //获取消息内容，JSON格式
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.avos.avoscloud.Data"));
+
+        } catch (JSONException e) {
+            Log.v(TAG, "JSONException: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
